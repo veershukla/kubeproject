92cf897b8dd1aaf2da9beda7ffbe9f451c1cba5d
Veerendra Shukla <veshukla@gmail.com>
2026-05-11 04:33:14 -0700

    message updated again

--- app/index.js
+++ app/index.js
@@ -3,5 +3,5 @@ const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ message: 'Hello from Kubernetes123!', version: '1.0.0' }));
+  res.end(JSON.stringify({ message: 'Hello from Kubernetes234!', version: '1.0.0' }));
 }).listen(PORT, () => console.log(`Server running on port ${PORT}`));
